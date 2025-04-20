e26259cdb617ab5513a5a71c132240303dbb530e
Luis Angel <angel.quispe.navarro@outlook.com>
2025-04-20 00:44:01 -0500

    Add user client in Contact

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 const Contact = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-green-100 via-white to-green-50 flex flex-col items-center justify-center px-4">
