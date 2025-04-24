6467e6134603d0c2f1111706180fbff6d2724436
Luis Angel <angel.quispe.navarro@outlook.com>
2025-04-23 23:54:41 -0500

    Prepared Vistas Terms and Contact

--- frontend/src/app/contact/page.tsx
+++ frontend/src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 const Contact = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-green-100 via-white to-green-50 flex flex-col items-center justify-center px-4">
+    <div className="min-h-screen bg-[#0A0B0C] flex flex-col items-center justify-center px-4">
       <h1 className="text-3xl font-bold  text-gray-800"> Contacto </h1>
     </div>
   );
--- frontend/src/app/terms/page.tsx
+++ frontend/src/app/terms/page.tsx
@@ -1,6 +1,6 @@
 const Terms = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-green-100 via-white to-green-50 flex flex-col items-center justify-center px-4">
+    <div className="min-h-screen bg-[#0A0B0C] flex flex-col items-center justify-center px-4">
       <h1 className="text-3xl font-bold  text-gray-800">Términos</h1>
     </div>
   );
