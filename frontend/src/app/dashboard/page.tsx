f8071e5c61537e5deb7513c9fcc6f8e9b04ca4c4
NikoDemo96 <chnicolas.aranguiz@gmail.com>
2025-04-24 22:49:46 -0400

    fix chatbot position in dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -43,38 +43,44 @@ const Dashboard = () => {
             />
           </div>
         </div>
+        <div className="flex gap-5">
 
-        <div className="gap-4 mt-4 flex flex-col items-center">
-          <h1 className="mt-3">Genera una petición</h1>
-          <Cardslider setMonths={setMonths} />
+          <div>
+            <div className="gap-4 mt-4 flex flex-col items-center">
+              <h1 className="mt-3">Genera una petición</h1>
+              <Cardslider setMonths={setMonths} />
 
-          <div
-            onClick={handleGenerate}
-            className="bg-black hover:bg-[#212226] border border-[#838383] h-8 mb-3 rounded-full w-50 text-[#EA5105] font-bold text-center cursor-pointer"
-          >
-            Generar Ahora
-          </div>
-        </div>
-
-        <div className="font-bold my-4 mb-6">
-          Resultado de la predicción:
-          {predictionResult && (
-            <div className="grid grid-cols-2 gap-4 mt-6">
-              {predictionResult.map((item: any, index: number) => (
-                <div key={index} className="bg-gray-900 p-4 rounded-xl">
-                  <h3 className="text-lg font-bold">
-                    {item.month} {item.year}
-                  </h3>
-                  <p className="text-sm mt-1">Emisiones: {item.emissions} kg CO₂</p>
-                  <p className="text-xs text-green-400">{item.advice}</p>
-                </div>
-              ))}
+              <div
+                onClick={handleGenerate}
+                className="bg-black hover:bg-[#212226] border border-[#838383] h-8 mb-3 rounded-full w-50 text-[#EA5105] font-bold text-center cursor-pointer"
+              >
+                Generar Ahora
+              </div>
             </div>
-          )}
+
+            <div className="font-bold my-4 mb-6">
+              Resultado de la predicción:
+              {predictionResult && (
+                <div className="grid grid-cols-2 gap-4 mt-6">
+                  {predictionResult.map((item: any, index: number) => (
+                    <div key={index} className="bg-gray-900 p-4 rounded-xl">
+                      <h3 className="text-lg font-bold">
+                        {item.month} {item.year}
+                      </h3>
+                      <p className="text-sm mt-1">
+                        Emisiones: {item.emissions} kg CO₂
+                      </p>
+                      <p className="text-xs text-green-400">{item.advice}</p>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+            <ScrollContainer />
+          </div>
+          <AiSuggestionsChat />
         </div>
-        <ScrollContainer />
       </div>
-      <AiSuggestionsChat />
     </div>
   );
 };
